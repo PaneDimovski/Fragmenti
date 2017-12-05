cd0c5e58f6553b66aa8eb99a4b8587a5578f2805
Anti <pane.dimovski@gmail.com>
2017-12-05 19:54:15 +0100

    Osmi Obid so slika i promena vo Fragment 1.

--- app/src/main/java/com/example/anti/navigacijafragmenti/Fragment1.java
+++ app/src/main/java/com/example/anti/navigacijafragmenti/Fragment1.java
@@ -7,6 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * Created by Anti on 12/5/2017.
@@ -14,13 +21,34 @@ import android.view.ViewGroup;
 
 public class Fragment1 extends Fragment {
 
+@BindView(R.id.textView)
+TextView txt;
+
+    private Unbinder mUnbinder;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment1, null);
+
+       mUnbinder= ButterKnife.bind(this,view);
+
+        txt.setText("Prviot Fragment 1");
             return  view;
 
+    }
+
+    @OnClick(R.id.button)
+    public  void buttonClick () {
+
+        Toast.makeText(getActivity(), "Toast Fragment 1", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mUnbinder.unbind();
     }
 }
